5709ca4fd406a82bf9942dbe83d9ccd0ac0b0a0f
Evandro <egzefer@gmail.com>
2016-09-14 00:13:42 -0300

    Request method changed from GET to POST

---
+++ src/main/java/com/egzefer/dto/PasswordRequestDTO.java
@@ -0,0 +1,21 @@
+package com.egzefer.dto;
+
+/**
+ * DTO class that have the password to be validated
+ * 
+ * @author evandro
+ *
+ */
+public class PasswordRequestDTO {
+
+	private String password;
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
